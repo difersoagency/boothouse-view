4c610ada4be5bd70aaa117242bad8ec374c3f08f
aadzhaan <33817970+aadzhaan@users.noreply.github.com>
2022-12-14 22:54:56 +0700

    tes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use DB;
 
 class AdminController extends Controller
 {
@@ -26,36 +27,46 @@ class AdminController extends Controller
         $tglmin = $r->tgl_min;
         $tglmax = $r->tgl_max;
 
-        $order = Order::where([['tgl_order', '>=', $tglmin],['tgl_order', '<=', $tglmax]])->addSelect(['count_booth_display' => function($q){
-            $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
-            ->from('detail_booth')
-            ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
-            ->where('detail_booth.jenis_booth_id', '=', '1')
-            ->whereColumn('detail_order.id', 'order.id')
-            ->groupBy('order.tgl_order');
-        },
-        'count_booth_outdoor' => function($q){
-            $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
-            ->from('detail_booth')
-            ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
-            ->where('detail_booth.jenis_booth_id', '=', '2')
-            ->whereColumn('detail_order.id', 'order.id')
-            ->groupBy('order.tgl_order');
-        },
-        'count_booth_portable' => function($q){
-            $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
-            ->from('detail_booth')
-            ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
-            ->where('detail_booth.jenis_booth_id', '=', '3')
-            ->whereColumn('detail_order.id', 'order.id')
-            ->groupBy('order.tgl_order');
-        }])->get();
+        $order = DB::table('detail_booth')
+        ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
+        ->join('order', 'order.id', '=', 'detail_order.order_id')
+        
+        ->whereRaw('tgl_order >= "'. $tglmin .'" AND tgl_order <= "'. $tglmax.'"')
+        ->selectRaw('order.tgl_order as tgl_order,
+        detail_booth.jenis_booth_id as id,
+        coalesce(count("detail_booth.jenis_booth_id"),0) as jumlah')
+        ->groupByRaw('order.tgl_order, detail_booth.jenis_booth_id')
+        ->get();
+        // $order = Order::where([['tgl_order', '>=', $tglmin],['tgl_order', '<=', $tglmax]])->addSelect(['count_booth_display' => function($q){
+        //     $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
+        //     ->from('detail_booth')
+        //     ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
+        //     ->where('detail_booth.jenis_booth_id', '=', '1')
+        //     ->whereColumn('detail_order.id', 'order.id')
+        //     ->groupBy('order.tgl_order');
+        // },
+        // 'count_booth_outdoor' => function($q){
+        //     $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
+        //     ->from('detail_booth')
+        //     ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
+        //     ->where('detail_booth.jenis_booth_id', '=', '2')
+        //     ->whereColumn('detail_order.id', 'order.id')
+        //     ->groupBy('order.tgl_order');
+        // },
+        // 'count_booth_portable' => function($q){
+        //     $q->selectRaw('coalesce(count("detail_booth.jenis_booth_id"),0)')
+        //     ->from('detail_booth')
+        //     ->join('detail_order', 'detail_order.detail_booth_id', '=', 'detail_booth.id')
+        //     ->where('detail_booth.jenis_booth_id', '=', '3')
+        //     ->whereColumn('detail_order.id', 'order.id')
+        //     ->groupBy('order.tgl_order');
+        // }])->get();
         $data = array();
-        foreach($order as $key => $res){
+        foreach($order as $res){
             $data['tgl_order'][$key] = $res->tgl_order;
-            $data['display'][$key] = $res->count_booth_display;
-            $data['portable'][$key] = $res->count_booth_portable;
-            $data['outdoor'][$key] = $res->count_booth_outdoor;
+            $data['id'][$key] = $res->id;
+            $data['count'][$key] = $res->jumlah;
+            // $data['outdoor'][$key] = $res->count_booth_outdoor;
         }
         return response()->json($data);
     }
